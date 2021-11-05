0f11d7de9f6ff5729730d14926eeb19e96c2093e
Gabriel Veloso <gabriel.azuirson@ufpe.br>
2021-11-05 18:12:31 -0300

    using saferesourceurl in live model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import {MatButtonModule} from '@angular/material/button';
 import { LivesListComponent } from './views/home/lives-list/lives-list.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatCardModule} from '@angular/material/card';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 import {MatDialogModule} from '@angular/material/dialog';
 import { LiveFormDialogComponent } from './views/home/live-form-dialog/live-form-dialog.component';
@@ -20,6 +19,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
--- src/app/views/home/lives-list/lives-list.component.ts
+++ src/app/views/home/lives-list/lives-list.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Live } from 'src/app/shared/model/live.model';
 import { LiveService } from 'src/app/shared/service/live.service';
+
 
 @Component({
   selector: 'app-lives-list',
   templateUrl: './lives-list.component.html',
-  styleUrls: ['./lives-list.component.css']
+  styleUrls: ['./lives-list.component.css'],
 })
 export class LivesListComponent implements OnInit {
 
-  livesPrevious: Live[];
-  livesNext: Live[];
+  livesPrevious: Live[] = [];
+  livesNext: Live[] = [];
 
-  constructor(public liveService: LiveService) { }
+  constructor(public liveService: LiveService, public sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.getLives();
@@ -22,12 +24,21 @@ export class LivesListComponent implements OnInit {
     this.liveService.getLivesWithFlag("previous").subscribe(lives=>{
       this.livesPrevious = lives.content;
       console.log(this.livesPrevious)
+
+      this.livesPrevious.forEach(live=>{
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
     });
 
     this.liveService.getLivesWithFlag("next").subscribe(lives=>{
       this.livesNext = lives.content;
-      console.log(this.livesNext)
+      console.log(this.livesNext);
+
+      this.livesNext.forEach(live=>{
+        live.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(live.liveLink);
+      });
     });
+    
   }
 
 }
